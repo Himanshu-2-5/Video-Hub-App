de8a1200f989704a190476e0684500c0b832443e
Himanshu Raj <hraj8864@gmail.com>
2023-01-25 21:50:38 +0530

    putting into gitHub

---
+++ src/Components/Home.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+        <Heading
+          textTransform={'uppercase'}
+          py={'2'}
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m={'auto'}
+        >
+          Services
+        </Heading>
+
+        <Stack
+        h={'full'}
+        p={'4'}
+        alignItems={'center'}
+        direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text letterSpacing={'widest'} lineHeight={'190%'} p={['4','16']} textAlign={'center'}>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img1} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img2} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img3} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+    <Box w={'full'} h={'100vh'}>
+      <Image src={img4} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
